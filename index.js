2c665b82f814a9e4b77cdc47b3c8512fbe4cf5b6
Adam <adam.crossland@zero1.co.uk>
2023-05-17 12:53:43 +0000

    work on module

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const { promises: fs } = require('fs');
+const Airtable = require('airtable');
 
+const COMPOSER_LOCK = 'composer.lock';
+const AIRTABLE_MAX_CHUNK = 10;
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const apiKey = core.getInput('apiKey');
+    const base = core.getInput('base');
+    const site = core.getInput('site');
+
+    if(!apiKey){
+      throw new Error('"apiKey" not provided');
+    }
+    if(!base){
+      throw new Error('"base" not provided');
+    }
+    if(!site){
+      throw new Error('"site" not provided');
+    }
+
+
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
